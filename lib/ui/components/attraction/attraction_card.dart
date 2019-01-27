750e6ad209b9b314a3f932b58e6a804b6d3dadf1
Wahyu Aditya Nugraha <wahyu.nugraha@online-pajak.com>
2019-01-27 21:52:18 +0700

    Tidying up attraction page

--- lib/ui/components/attraction/attraction_card.dart
+++ lib/ui/components/attraction/attraction_card.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class AttractionCard extends StatelessWidget {
 
@@ -12,15 +12,50 @@ class AttractionCard extends StatelessWidget {
     return GestureDetector(
         onTap: () {},
         child: Container(
-          padding: EdgeInsets.all(10.0),
-          height: 200.0,
+          padding: EdgeInsets.all(5.0),
           child: ClipRRect(
               borderRadius: BorderRadius.circular(6.0),
-              child: FadeInImage.assetNetwork(
-                image: document["image"],
-                fit: BoxFit.cover,
-                height: 400,
-                placeholder: "assets/images/nophoto.gif",
+              child: Stack(
+                children: <Widget>[
+                  FadeInImage.assetNetwork(
+                    image: document["image"],
+                    fit: BoxFit.cover,
+                    height: 400,
+                    placeholder: "assets/images/nophoto.gif",
+                  ),
+                  Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(200, 0, 0, 0),
+                                  Color.fromARGB(0, 0, 0, 0)
+                                ],
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.center,
+                              )
+                          ),
+                          width: MediaQuery
+                              .of(context)
+                              .size
+                              .width,
+                          height: MediaQuery
+                              .of(context)
+                              .size
+                              .height,
+                          padding: EdgeInsets.only(bottom: 20.0),
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Text(document["name"], style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Chewy',
+                                fontWeight: FontWeight.w200,
+                                fontSize: 14.0),),
+                          )
+                      )
+                  )
+                ],
               )
           ),
         )
