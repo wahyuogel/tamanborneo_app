5d388e8ed07192f2b36dbba0a1582d8976143f7f
Wahyu Aditya Nugraha <wahyu.nugraha@online-pajak.com>
2019-01-30 01:05:56 +0700

    Fix map helper file naming convention

--- lib/ui/components/home/home_carousel.dart
+++ lib/ui/components/home/home_carousel.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:zoo_app/helper/MapHelper.dart';
+import 'package:zoo_app/helper/map_helper.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
