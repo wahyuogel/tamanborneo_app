f35bf6a6d0c610bf44edddf8216ef2e690332755
Wahyu Aditya Nugraha <wahyu.nugraha@online-pajak.com>
2019-01-30 00:27:38 +0700

    Add gesture detector for promo banner home

--- lib/ui/components/home/home_carousel.dart
+++ lib/ui/components/home/home_carousel.dart
@@ -33,45 +33,50 @@ class _HomeCarouselState extends State<HomeCarousel> {
   }
 
   Widget _buildCarouselItem(DocumentSnapshot document){
-    return Container(
-        margin: EdgeInsets.all(3.0),
-        child: ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(5.0)),
-            child: Stack(
-              children: <Widget>[
-                Image.network(document["image"],
-                  fit: BoxFit.fitWidth,
-                  width: 1000.0,
-                ),
-                Positioned(
-                    bottom: 0.0,
-                    left: 0.0,
-                    right: 0.0,
-                    child: Container(
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [
-                                Color.fromARGB(200, 0, 0, 0),
-                                Color.fromARGB(0, 0, 0, 0)
-                              ],
-                              begin: Alignment.bottomCenter,
-                              end: Alignment.topCenter,
-                            )
-                        ),
-                        padding: EdgeInsets.symmetric(
-                            vertical: 10.0, horizontal: 20.0),
-                        child: Text(document["name"],
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w600,
-                              fontSize: 18.0),
-                        )
-                    )
-                ),
-              ],
-            )
-        )
+    return GestureDetector(
+      child: Container(
+          margin: EdgeInsets.all(3.0),
+          child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              child: Stack(
+                children: <Widget>[
+                  Image.network(document["image"],
+                    fit: BoxFit.fitWidth,
+                    width: 1000.0,
+                  ),
+                  Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(200, 0, 0, 0),
+                                  Color.fromARGB(0, 0, 0, 0)
+                                ],
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                              )
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 20.0),
+                          child: Text(document["name"],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18.0),
+                          )
+                      )
+                  ),
+                ],
+              )
+          )
+      ),
+      onTap: (){
+
+      },
     );
   }
 
